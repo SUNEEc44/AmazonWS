ff4f7ac60a198020a77737fa5453010bbe811c26
svaduka <sainagaraju.vaduka@gmail.com>
2016-05-29 18:40:35 +0530

    Updated

--- AmazonWS/src/com/amazon/CharacterCount.java
+++ AmazonWS/src/com/amazon/CharacterCount.java
@@ -10,22 +10,100 @@ public class CharacterCount {
 	 *  Given that output string's length is always smaller than input string, 
 	 *  you have do it inplace. No extra space , no arrays should be used
 	 */
+	final char CHAR_CONSTANT='|';
 	public static void main(String[] args) 
 	{
-		final String str="aaabbbcc";
+		final String str="aabbdddddddddedffffff                   fffffffffffffffffffffffsdfsfsffg";
 		CharacterCount counter=new CharacterCount();
-		int count=counter.countCharInStr('b', str);
-		System.out.println("count:"+count);
+//		int count=counter.countCharInStr('b', str);
+//		System.out.println("count:"+count);
+		
+		String output=counter.countEachCharFrequency(str);
+		
+		System.out.println(output);
 	}
 	
 	public String countEachCharFrequency(final String  word)
+ {
+		boolean checkAnyRepeatedChar = Boolean.TRUE;
+		StringBuffer computedOutput = null;
+
+		if (checkAnyRepeatedChar) {
+			computedOutput = new StringBuffer();
+			char searchChar ;
+			do {
+				searchChar = CHAR_CONSTANT;
+				searchChar = nextSearchChar(word, computedOutput);
+
+				if (searchChar != CHAR_CONSTANT) 
+				{
+					int searchCount = countCharInStr(searchChar, word);
+					computedOutput.append(searchChar);
+					computedOutput.append(searchCount);
+				}
+			} while (searchChar != CHAR_CONSTANT);
+		}
+		System.out.println("Computed Output:"+computedOutput);
+		return computedOutput!=null && computedOutput.length()<=word.length()?computedOutput.toString():"";
+		
+	}
+	
+	public char nextSearchChar(final String word, StringBuffer result){
+		char searchChar=CHAR_CONSTANT;
+		char tempChar=CHAR_CONSTANT;
+		boolean isSearchCharAlreadyComputed=Boolean.FALSE;
+		for (int i = 0; i < word.length(); i++) 
+		{
+			tempChar=word.charAt(i);
+			isSearchCharAlreadyComputed=checkCharExistInStr(tempChar, result);
+		
+			if(!isSearchCharAlreadyComputed)
+			{
+				searchChar=tempChar;
+				break;	
+			}
+		}
+		return searchChar;
+	}
+	public boolean checkCharExistInStr(final char searchChar,final StringBuffer result)
 	{
-		return null;
+		boolean isAlreadyComputed=Boolean.FALSE;
+	
+		for (int i = 0; i < result.length(); i++) {
+			if(searchChar==result.charAt(i)){
+				isAlreadyComputed=Boolean.TRUE;
+				break;
+			}
+		}
+		
+		return isAlreadyComputed;
 	}
 
-	public boolean validateStr(final String word)
+	public boolean validateAnyCharRepeated(final String word)
+	
 	{
-		return Boolean.TRUE;
+		boolean proceedWithCount=Boolean.FALSE;
+		
+		boolean isAnyCharRepeted=Boolean.FALSE;
+		char repeatedChar=CHAR_CONSTANT;
+		String nextStr=null;
+		int iReapetedCount=0;
+		for (int i = 0; i < word.length(); i++) 
+		{
+			repeatedChar=word.charAt(i);
+			if(i<word.length()-1)
+			{
+			nextStr=word.substring(i+1);
+			isAnyCharRepeted=checkCharExistInStr(repeatedChar, new StringBuffer(nextStr));
+			if(isAnyCharRepeted){
+				iReapetedCount++;
+			}
+			}
+		}
+		if(iReapetedCount>=(word.length()/2)){
+			proceedWithCount=Boolean.TRUE;
+		}
+		return proceedWithCount;
 	}
 	
 	public int countCharInStr(final char searchChar, final String word)
@@ -37,7 +115,7 @@ public class CharacterCount {
 				return 0;
 			}
 		}
-		char tempSearchChar = '0';// Non Character
+		char tempSearchChar = CHAR_CONSTANT;// Non Character
 		String remainingWord = null;
 	
 		for (int i = 0; i < word.length(); i++) 
