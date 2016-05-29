6556066a0cb7d52dfc193af6d10b804fe838e055
svaduka <sainagaraju.vaduka@gmail.com>
2016-05-29 16:12:56 +0530

    Working for Amazon Interview

---
+++ AmazonWS/src/com/amazon/CharacterCount.java
@@ -0,0 +1,52 @@
+package com.amazon;
+
+public class CharacterCount {
+
+	/**
+	 * @author Sainagaraju Vaduka
+	 * 
+	 *  Given a string "aaabbbcc", 
+	 *  compress it, = "a3b3c2" . 
+	 *  Given that output string's length is always smaller than input string, 
+	 *  you have do it inplace. No extra space , no arrays should be used
+	 */
+	public static void main(String[] args) 
+	{
+		final String str="aaabbbcc";
+		CharacterCount counter=new CharacterCount();
+		int count=counter.countCharInStr('b', str);
+		System.out.println("count:"+count);
+	}
+
+	public boolean validateStr(final String word)
+	{
+		return Boolean.TRUE;
+	}
+	
+	public int countCharInStr(final char searchChar, final String word)
+	{
+		if (word.length() == 1) {
+			if (word.charAt(0) == searchChar) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+		char tempSearchChar = '0';// Non Character
+		String remainingWord = null;
+	
+		for (int i = 0; i < word.length(); i++) 
+		{
+			tempSearchChar = word.charAt(i);
+			
+			if (tempSearchChar == searchChar) 
+			{
+				remainingWord = word.substring(i + 1, word.length());
+				
+				return 1+countCharInStr(searchChar, remainingWord);
+			}
+		}
+
+		return 0;
+	}
+}
